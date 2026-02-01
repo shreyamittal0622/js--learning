eeb5e1b289401ac51056ed6e4b0e963769063eca
shreyamittal0622 <shreyamittal0622@gmail.com>
2026-02-01 16:12:46 +0000

    summary-datatype

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -20,3 +20,23 @@ console.log(typeof(myfun)) //function object
 
 //primitive
 //string,number,null,undefined,boolean
+
+
+
+//-------memory--------
+//stack(primitive) , heap(non-primitive)
+
+
+let myname ="shreya"
+let anotherName = myname
+anotherName="cheshta"
+console.log(myname) //shreya
+console.log(anotherName)  // cheshta
+  // original value is not changed 
+  // gets stacked up one on another = myname-> anotherName->myname
+  //in heap every change is made in original value only like-
+  let userOne={
+    email: "shreya@.com",
+    upi: "user@ybl"
+  }// anything can be accesse and changed and it refers to heap
+  

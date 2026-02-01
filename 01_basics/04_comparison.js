67fa82bf7615e98ea62ab3e71c27bd5c26a474d4
shreyamittal0622 <shreyamittal0622@gmail.com>
2026-02-01 15:11:33 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log("02">1)
+console.log(2>1)
+
+console.log(null>0)
+console.log(null>=0)
+console.log(null==0)
+
+// undefined gives false in all 3 cases
+
+// === - strict check 
+console.log("2"===2)

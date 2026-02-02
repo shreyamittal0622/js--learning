07771be6247afbd2a0027c01af36e35fcd14d824
shreyamittal0622 <shreyamittal0622@gmail.com>
2026-02-02 14:43:12 +0000

    objeects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,14 @@
+
+
+const obj1 ={ 1:"a", 2:"b"}
+const obj2 ={ 3:"a", 4:"b"}
+const obj3 ={ 5:"a", 6:"b"}
+
+const obj =Object.assign({}, obj1, obj2, obj3)
+console.log(obj);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const obj_another ={...obj1, ...obj2}
+console.log(obj_another) //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+console.log(Object.keys(obj))
+//we can even access values and entries in same way 

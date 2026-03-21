f044975ef4facf0884d66e16f082f76510fe21f5
shreyamittal0622 <shreyamittal0622@gmail.com>
2026-03-21 09:32:53 +0530

    Refactor variables and add comments on var usage
    Updated city and password variables, added comments on var usage.

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,9 @@ const email="hello@gmail.com"
 let city="faridabad"
 var password="1212"
 let state;
+/* prefer not to use var
+because of block scope and functional scope
+*/
 city="dhanbad"
 password="1234"
 console.table([email,city,password,state])

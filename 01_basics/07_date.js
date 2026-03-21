93b8a4377af5b003ca21636ee5420cdf498bd3d4
shreyamittal0622 <shreyamittal0622@gmail.com>
2026-03-21 16:40:58 +0530

    Update 07_date.js

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -1,6 +1,6 @@
 let myDate = new Date()
 console.log(myDate.toString())
-console.log(myDate.toDateString())
+console.log(myDate.toDateString()) //
 console.log(myDate.toLocaleString())
 
 console.log(typeof(myDate));  //object
@@ -14,7 +14,8 @@ console.log(anotherDate.toDateString())
 let newcreatedDate = new Date ("01-14-2025") 
 let myTimeStamp = Date.now()
 
-console.log(myTimeStamp);
+console.log(myTimeStamp); 
+// it gives in ms so for that -console.log(Math.floor(Date.now()/1000))
 console.log(newcreatedDate.getTime());
 
 
